9928a53eda79cdf1f11ae389d77a554e18ad0ad5
Agent-h <hadrien.milano@gmail.com>
2014-10-14 17:02:28 +0200

    and again

--- scripts/create-avd.sh
+++ scripts/create-avd.sh
@@ -7,4 +7,4 @@ echo "Creating avd for android-$targetSDK"
 
 # must android will ask if we want to create custom hardware with default 'no'.
 # so we send a return keypress to say no.
-echo '' | android create avd -f -n "avd_android-$targetSDK" -t "android-$targetSDK" --abi android-wear/armeabi-v7a
+echo '' | android create avd -f -n "avd_android-$targetSDK" -t "android-$targetSDK" --abi armeabi-v7a
