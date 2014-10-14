23a95a6ec2318c2e0e49d5d815e6bea7885904a0
Hadrien Milano <hadrien.milano@gmail.com>
2014-10-14 16:07:42 +0200

    create avd scripts

---
+++ scripts/create-avd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+targetSDK=20
+avd_options='--skin WVGA800'
+
+echo "Creating avd for android-$targetSDK"
+
+# must android will ask if we want to create custom hardware with default 'no'.
+# so we send a return keypress to say no.
+echo '' | ./android.sh create avd -f -n "avd_android-$targetSDK" -t "android-$targetSDK" --abi android-wear/armeabi-v7a
