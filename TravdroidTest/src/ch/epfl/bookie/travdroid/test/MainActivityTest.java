db164b41bd6daa4218d6943da20b39f97cbe490e
Hadrien Milano <hadrien.milano@gmail.com>
2014-10-14 22:48:27 +0200

    add android application and test application

---
+++ TravdroidTest/src/ch/epfl/bookie/travdroid/test/MainActivityTest.java
@@ -0,0 +1,15 @@
+package ch.epfl.bookie.travdroid.test;
+
+import ch.epfl.bookie.travdroid.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+    
+    public void testHello() {
+        fail("Not yet implemented");
+    }
+}
