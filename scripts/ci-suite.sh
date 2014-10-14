ecdabba3358454d54465ac7ade2c34023b28878a
Hadrien Milano <hadrien.milano@gmail.com>
2014-10-14 22:13:29 +0200

    refactoring

--- scripts/ci-suite.sh
+++ scripts/ci-suite.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-./create-avd.sh
+emulator -avd avd-19 -no-skin -no-audio & #-no-window &
+android-wait-for-emulator
+adb shell input keyevent 82 &
--- scripts/create-avd.sh
+++ scripts/create-avd.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
+# create-avd.sh
+# @author Hadrien Milano <hadrien.milano@epfl.ch>
+#
+# Creates an avd for the target android sdk
 
-targetSDK=19
-avd_options='--skin WVGA800'
+base_name="avd"
 
-echo "Creating avd for android-$targetSDK"
+if [ -z "$1" ]; then
+  cat 1>&2 <<EOF
+Usage: create-avd XX [base_name] [options]
+ - XX: sdk version to use for this emulator (19 for instance)
+ - base_name: name to use for this emulator (default: $base_name)
+Creates an AVD named base_name-XX with default hardware configuration running
+on armeabi-v7a
+EOF
+exit 1
+fi
+target_sdk="$1"
+shift
+if [ ! -z "$1" ]; then base_name="$1" ; fi
+shift
+if [ ! -z "$1" ]; then avd_options="$@" ; fi
 
-# must android will ask if we want to create custom hardware with default 'no'.
+echo "Creating avd for android-$target_sdk"
+
+# android will ask if we want to create custom hardware with default 'no'.
 # so we send a return keypress to say no.
-echo '' | android create avd -f -n "avd_android-$targetSDK" -t "android-$targetSDK" --abi armeabi-v7a
+echo 'no' | android create avd --force -n "$base_name-$target_sdk" -t "android-$target_sdk" --abi armeabi-v7a $avd_options
