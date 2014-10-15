7fa7c25ae580362c495d38d21dfe031175657937
Hadrien Milano <hadrien.milano@gmail.com>
2014-10-15 09:21:30 +0200

    trying to fix build

--- scripts/ci-suite.sh
+++ scripts/ci-suite.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 ant_cmd="ant clean emma debug install test"
+adt_sdk=`which android.bat | sed "s/\/tools\/android.bat$//"`
+
+echo "sdk location: $adt_sdk"
 
 # Debug information
 echo "printing android-wait-for-emulator"
 cat `which android-wait-for-emulator`
 # start emulator
-#emulator -avd avd-19 -no-skin -no-audio -no-window &
+emulator -avd avd-19 -no-skin -no-audio -no-window &
 #android-wait-for-emulator
 
 # start tests
-android update project --path Travdroid --target android-19
-android update project --path TravdroidTest --target android-19
-android update test-project --path TravdroidTest --main ../TravdroidTest
+echo "sdk.dir=$adt_sdk" | tee TravdroidTest/local.properties | tee Travdroid/local.properties
+
 cd TravdroidTest
 echo "Running $ant_cmd"
 result=`$ant_cmd`
