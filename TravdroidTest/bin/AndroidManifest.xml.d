9e7a126bf437b731c506be1950663e2b973b0b02
Hadrien Milano <hadrien.milano@gmail.com>
2014-10-15 08:16:32 +0200

    not commiting bin folders

--- TravdroidTest/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-d:\workspace\travis-sweng-test\TravdroidTest\bin\AndroidManifest.xml : \
-d:\workspace\travis-sweng-test\TravdroidTest\AndroidManifest.xml \
--- TravdroidTest/bin/TravdroidTest-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-d:\workspace\travis-sweng-test\TravdroidTest\bin\TravdroidTest-debug-unaligned.apk : \
-d:\workspace\travis-sweng-test\TravdroidTest\bin\TravdroidTest.ap_ \
-d:\workspace\travis-sweng-test\TravdroidTest\bin\classes.dex \
-d:\workspace\travis-sweng-test\TravdroidTest\libs\robotium-solo-5.2.1.jar \
